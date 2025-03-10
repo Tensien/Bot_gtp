845ca8332ddbbdb21c7c3c1814672eec353564cf
Tim Guzov <61355255+Tensien@users.noreply.github.com>
2025-03-10 20:29:24 +0400

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from threading import Thread
 
@@ -8,7 +9,7 @@ def home():
     return "I'm alive!"
 
 def run():
-    port = int(os.environ.get("PORT", 8080))  # Получаем порт из переменной окружения
+    port = int(os.environ.get("PORT", 10000))  # Получаем порт из переменной окружения
     app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
